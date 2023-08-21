4b8b886d403eef70ef95f46e24a0743e9be725db
Donovan Nguyen <shiiirro.donovan@gmail.com>
2023-08-20 20:49:58 -0700

    prototype

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Visualizer from './components/Visualizer';
 
 function App() {
   return (
     <div className="App">
-      
+      <Visualizer/>
     </div>
   );
 }
